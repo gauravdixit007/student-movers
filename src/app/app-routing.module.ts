6630e6bcb1d519a09af64f70325bd6db4643fdeb
Gaurav Dixit <gaurav.techie.engineer@gmail.com>
2019-06-18 20:06:45 +0530

    Setup route for jobs List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { JobsListComponent } from './jobs-list/jobs-list.component';
 
 const routes: Routes = [
-  {path: 'employees', component: EmployeesListComponent}
+  {path: 'employees', component: EmployeesListComponent},
+  {path: 'jobs', component: JobsListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { JobsListComponent } from './jobs-list/jobs-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesListComponent
+    EmployeesListComponent,
+    JobsListComponent
   ],
   imports: [
     BrowserModule,
