bf6ee7d62f735035115b17244723b82d806bf381
Gaurav Dixit <gaurav.techie.engineer@gmail.com>
2019-06-18 19:27:56 +0530

    Configured routing for the second page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employees', component: EmployeesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
