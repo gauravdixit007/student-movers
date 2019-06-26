ff5ef8832d51b445a8858a6e84ec6571b3fffc1a
Gaurav Dixit <gaurav.techie.engineer@gmail.com>
2019-06-26 09:33:29 +0530

    Initial commit for Employees list page

--- src/app/employees-list/employees-list.component.ts
+++ src/app/employees-list/employees-list.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesListComponent implements OnInit {
 
+  public job = {
+      name: 'Natasha Andrade',
+      address: '#4830 Hummingbird Way Cambridge, MA 02142',
+      employees: [
+        {
+          name: 'Thomas Mumford',
+          hours: 3.5,
+          phone: '781-234-5698'
+        }
+      ]
+  }
+
   constructor() { }
 
   ngOnInit() {
